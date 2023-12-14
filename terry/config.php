e49f58b79c2977413d8261aa5a8d8afba4c0812f
Terry <69320410+yrrxt@users.noreply.github.com>
2023-12-14 11:20:54 +0300

    project

---
+++ terry/config.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'Train Reservation System';
+//E-Ticketing System For Railway
+$supervisor_name = "Dr. Simon Chege";
+$developer_name = "Terry Nyakio";
+$developer_matric = "16/52HA016";
---
+++ terry/index.php
@@ -0,0 +1,431 @@
+﻿<!DOCTYPE html>
+<?php
+include 'constants.php';
+?>
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo @$title; ?></title>
+
+
+    <!-- [ FONT-AWESOME ICON ] 
+        
+=========================================================================================================================-->
+
+    <link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+
+    <!-- [ PLUGIN STYLESHEET ]
+        
+=========================================================================================================================-->
+
+    <link rel="shortcut icon" type="image/x-icon" href="images/icon.png">
+
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+
+    <link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+
+    <!-- [ Boot STYLESHEET ]
+        
+=========================================================================================================================-->
+
+    <link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+
+    <link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+
+    <!-- [ DEFAULT STYLESHEET ] 
+        
+=========================================================================================================================-->
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+    <link rel="stylesheet" type="text/css" href="css/color/themecolor.css">
+
+
+</head>
+
+<body>
+
+    <!-- [ LOADERs ]
+
+================================================================================================================================-->
+
+    <div class="preloader">
+
+        <div class="loader theme_background_color">
+
+            <span></span>
+
+
+        </div>
+    </div>
+    <!-- [ /PRELOADER ]
+
+=============================================================================================================================-->
+
+    <!-- [WRAPPER ]
+
+=============================================================================================================================-->
+
+    <div class="wrapper">
+
+        <!-- [NAV]
+ 
+============================================================================================================================-->
+
+        <!-- Navigation
+    ==========================================-->
+
+        <nav class=" nim-menu navbar navbar-default navbar-fixed-top">
+
+            <div class="container">
+
+                <!-- Brand and toggle get grouped for better mobile display -->
+
+                <div class="navbar-header">
+
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+
+                        <span class="sr-only">Toggle navigation</span>
+
+                        <span class="icon-bar"></span>
+
+                        <span class="icon-bar"></span>
+
+                        <span class="icon-bar"></span>
+
+                    </button>
+
+                    <a class="navbar-brand" href="index.php">TRAIN RESERVATION SYSTEM<span class="themecolor">
+                            </a>
+
+                </div>
+
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                    <ul class="nav navbar-nav navbar-right">
+
+                        <li><a href="#home" class="page-scroll">
+                                <h3>HOME</h3>
+                            </a></li>
+
+                        <li><a href="#two" class="page-scroll">
+                                <h3>ABOUT</h3>
+                            </a></li>
+
+                        <li><a href="library/map.html" class="page-scroll">
+                                <h3>MAP</h3>
+                            </a></li>
+
+                        <li><a href="pro/signin.php" class="page-scroll">
+                                <h3>LOGIN</h3>
+                            </a></li>
+
+                        <li><a href="pro/adminsignin.php" class="page-scroll">
+                                <h3>ADMIN</h3>
+                            </a></li>
+                    </ul>
+
+                </div>
+                <!-- /.navbar-collapse -->
+
+            </div><!-- /.container-fluid -->
+
+        </nav>
+
+
+
+        <!-- [/NAV]
+ 
+============================================================================================================================-->
+
+
+        <!-- [/MAIN-HEADING]
+ 
+============================================================================================================================-->
+
+        <section class="main-heading" id="home">
+
+            <div class="overlay">
+
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="main-heading-content col-md-12 col-sm-12 text-center">
+
+                            <h1 class="main-heading-title"><span class="main-element themecolor"
+                                    data-elements=" NAIROBI, NAIROBI, NAIROBI"></span></h1>
+
+                            <h1 class="main-heading-title"><span class="main-element themecolor"
+                                    data-elements=" RAILWAYS, RAILWAYS, RAILWAYS"></span>
+                            </h1>
+
+                            <p class="main-heading-text">WELCOME TO,<br />Nairobi Railways</p>
+
+                            <div class="btn-bar">
+
+                                <a href="pro/signin.php" class="btn btn-custom theme_background_color">MAKE A RESERVATION</a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </section>
+
+        <section class="main-heading" id="home">
+
+            <div class="overlay">
+
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="main-heading-content col-md-12 col-sm-12 text-center">
+
+                            <h1 class="main-heading-title">Nairobi Railways</h1>
+
+                            <p class="main-heading-text">The old Kenyan railway, also known as the Uganda Railway or the Lunatic Line, was built by the British colonial government between 1896 and 1901.
+                            It connected Mombasa to Kisumu and later to Uganda.The metre-gauge network, as known in Kenya, is the older one, dating back to the colonial era. It connects Mombasa to the Ugandan border, and also branches to other destinations such as Nanyuki, Kisumu, and Magadi.
+                            By all means and at all costs, the line was destined to happen.
+                            Syokimau, a Kamba medicine woman and prophetess had foretold the coming of a long iron snake belching fire and smoke as it slithered from the Indian Ocean and winding its way across the plains and highlands to an unknown destination.
+                            As she divulged her prophecy, she said strangers with red wiry hair would come with the snake and they would bring change. Not even the British aristocrats who were feuding within the precincts of the House of Commons could stop it.
+                            The Uganda Railway emerged as one of the most famous railway lines in the world. Interestingly, it is the only railway line which has more than one name. It was christened the Uganda Railway then later referred to as the Permanent Way, The Iron Snake and most famously as the Lunatic Line.
+                            The line had such a profound effect on our country and is the greatest contributor to the fruits of economic prosperity we enjoy today. It is this railway that extensively epitomizes the birth and growth of Kenya as a nation. In the wake of its path during construction, it gave birth to settlements and encouraged trade. 
+                            After completion of construction, development blueprints were planned along the lines path leading to the establishment of the Northern Corridor; the region’s trade route with the highest economic potential.
+                            </p>
+
+                            <div class="btn-bar">
+
+                                <a href="system/" class="btn btn-custom theme_background_color">Get Started</a>
+
+                                <a href="system/admin" class="btn btn-custom-outline">Admin</a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </section>
+
+
+        <!-- [/MAIN-HEADING]
+ 
+============================================================================================================================-->
+
+
+
+        <!-- [ABOUT US]
+ 
+============================================================================================================================-->
+
+        <section class="aboutus white-background black" id="two">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-12 text-center black">
+
+                        <h3 class="title">ABOUT US</h3>
+
+                        <p class="a-slog">Developed By Terry Nyakio Nzioka
+                            and Supervised By Dr. Simon Chege</p>
+
+                    </div>
+
+                </div>
+
+                <div class="gap">
+
+
+                </div>
+
+
+                <div class="row about-box">
+
+                    <div class="col-sm-4 text-center">
+
+                        <div class="margin-bottom">
+
+                            <i class="fa fa-mobile-phone"></i>
+
+                            <h4>Get Train Tickets from the comfort of your home</h4>
+
+                            <p class="black">Book train tickets from anywhere using the robust ticketing platform
+                                exclusively built to provide the passengers with pleasant ticketing experience. </p>
+
+                        </div> <!-- / margin -->
+
+                    </div> <!-- /col -->
+
+                    <div class="col-sm-4 about-line text-center">
+
+                        <div class="margin-bottom">
+
+                            <i class="fa fa-sliders"></i>
+
+                            <h4>Train & Ticketing related information at your fingertips</h4>
+
+                            <p class="black">Checkout for route information, fare information on real time
+                                basis with the Platform.</p>
+
+                        </div> <!-- / margin -->
+
+                    </div><!-- /col -->
+
+                    <div class="col-sm-4 text-center">
+
+                        <div class="margin-bottom">
+
+                            <i class="fa fa-money"></i>
+
+                            <h4>Pay Convniently</h4>
+
+                            <p class="black">Online payment</p>
+
+                        </div> <!-- / margin -->
+
+                    </div><!-- /col -->
+
+                </div> <!-- /row -->
+
+
+
+
+
+            </div>
+        </section>
+
+
+
+        <footer class="site-footer section-spacing text-center " id="eight">
+
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-4">
+
+                        <p class="footer-links"><a href="#">Terms of Use</a> <a href="#">Privacy Policy</a></p>
+
+                    </div>
+
+                    <div class="col-md-4"> <small>&copy; <?php echo date('Y'); ?>, Developed By <?php echo $developer_name; ?> </small></div>
+
+                    <div class="col-md-4">
+
+                        <!--social-->
+
+                        
+            <ul class="social">
+
+              <li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter "></i></a></li>
+
+              <li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+
+              <li><a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
+
+            </ul>
+
+
+                        <!--social end-->
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </footer>
+
+
+
+
+        <!-- [/FOOTER]
+ 
+============================================================================================================================-->
+
+
+
+
+    </div>
+
+
+    <!-- [ /WRAPPER ]
+
+=============================================================================================================================-->
+
+
+    <!-- [ DEFAULT SCRIPT ] -->
+
+    <script src="library/modernizr.custom.97074.js"></script>
+
+    <script src="library/jquery-1.11.3.min.js"></script>
+
+    <script src="library/bootstrap/js/bootstrap.js"></script>
+
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+
+    <!-- [ PLUGIN SCRIPT ] -->
+
+    <script src="library/vegas/vegas.min.js"></script>
+
+    <script src="js/plugins.js"></script>
+
+    <!-- [ TYPING SCRIPT ] -->
+
+    <script src="js/typed.js"></script>
+
+    <!-- [ COUNT SCRIPT ] -->
+
+    <script src="js/fappear.js"></script>
+
+    <script src="js/jquery.countTo.js"></script>
+
+    <!-- [ SLIDER SCRIPT ] -->
+
+    <script src="js/owl.carousel.js"></script>
+
+    <script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+
+    <script type="text/javascript" src="js/SmoothScroll.js"></script>
+
+
+    <!-- [ COMMON SCRIPT ] -->
+    <script src="js/common.js"></script>
+
+</body>
+
+
+</html>
